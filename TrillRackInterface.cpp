56edb4f5906027232aab04a8c8566b761fb7614d
Giulio Moro <giuliomoro@yahoo.it>
2021-01-22 16:49:47 +0000

    Added scope capabilities to TrillRackInterface

--- TrillRackInterface.cpp
+++ TrillRackInterface.cpp
@@ -1,13 +1,21 @@
 #include "TrillRackInterface.h"
+#include <string.h>
 
 TrillRackInterface::TrillRackInterface(unsigned int anInCh, unsigned int anOutCh0, unsigned int anOutCh1)
 {
+	setup(anInCh, anOutCh0, anOutCh1);
+}
+
+int TrillRackInterface::setup(unsigned int anInCh, unsigned int anOutCh0, unsigned int anOutCh1)
+{
+	scopeInited = false;
 	this->anInCh = anInCh;
 	this->anOutCh[0] = anOutCh0;
 	this->anOutCh[1] = anOutCh1;
 	anIn = 0;
-	for(unsigned int n = 0; n < nAnOut; ++n)
-		anOut[n] = 0;
+	memset(scopeData, 0, sizeof(scopeData));
+	memset(anOut, 0, sizeof(anOut));
+	return 0;
 }
 
 float TrillRackInterface::analogRead()
@@ -21,8 +29,25 @@ void TrillRackInterface::analogWrite(unsigned int channel, float val)
 		anOut[channel] = val;
 }
 
+void TrillRackInterface::scopeWrite(float* data)
+{
+	memcpy(scopeData, data, sizeof(scopeData));
+}
+
+void TrillRackInterface::scopeWrite(unsigned int channel, float val)
+{
+	if(channel < kScopeChannels)
+		scopeData[channel] = val;
+}
 void TrillRackInterface::process(BelaContext* context)
 {
+	if(!scopeInited) {
+		// we initialise here so setup and constructor don't need context
+		scope.setup(kScopeChannels, context->audioSampleRate / context->audioFrames);
+		scopeInited = true;
+	}
+	else
+		scope.log(scopeData);
         if(anInCh < context->analogInChannels)
                 anIn = ::analogRead(context, 0, anInCh);
 	for(unsigned int i = 0; i < nAnOut; ++i)
--- TrillRackInterface.h
+++ TrillRackInterface.h
@@ -1,19 +1,28 @@
 #pragma once
 #include <Bela.h>
+#include <libraries/Scope/Scope.h>
 
 bool tr_setup();
 void tr_loop();
 class TrillRackInterface
 {
 public:
+	TrillRackInterface() {};
 	TrillRackInterface(unsigned int anInCh, unsigned int anOut0Ch, unsigned int anOut1Ch);
+	int setup(unsigned int anInCh, unsigned int anOutCh0, unsigned int anOutCh1);
 	float analogRead();
 	void analogWrite(unsigned int channel, float val);
 	void process(BelaContext* context);
+	void scopeWrite(unsigned int channel, float val);
+	void scopeWrite(float* val);
+	enum {kScopeChannels = 4};
 private:
 	enum { nAnOut = 2 };
 	float anIn;
 	float anOut[nAnOut];
 	unsigned int anInCh;
 	unsigned int anOutCh[nAnOut];
+	float scopeData[kScopeChannels];
+	Scope scope;
+	bool scopeInited;
 };
