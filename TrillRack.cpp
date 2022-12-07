76a2a9a3aae0f60a885db2ad3e93b03d024972b7
Giulio Moro <giuliomoro@yahoo.it>
2022-12-07 20:28:05 +0100

    Send out zeros during mode change

--- TrillRack.cpp
+++ TrillRack.cpp
@@ -539,6 +539,14 @@ void tr_render(BelaContext* context)
 	{
 		ledSliders.process(trill.rawData.data());
 		performanceMode_render(context); // TODO: we should run the active mode even if we are in alt, but making sure the LEDs don't get set
+	} else {
+		// zero the outputs
+		gManualAnOut[0] = gManualAnOut[1] = 0;
+		for(size_t n = 0; n < context->analogFrames; ++n)
+		{
+			for(size_t c = 0; c < context->analogOutChannels; ++c)
+				analogWriteOnce(context, n, c, 0);
+		}
 	}
 	// actually display the updated LEDs
 	// this may have been written by alt, mode_setups or mode_renders, whatever last wrote it is whatever we display
