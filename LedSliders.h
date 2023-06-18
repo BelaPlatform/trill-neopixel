c5779f19afe4765af665d10921cf9a6e32e30968
Giulio Moro <giuliomoro@yahoo.it>
2023-06-19 00:45:32 +0200

    reduced default num of leds per centroid

--- LedSliders.h
+++ LedSliders.h
@@ -7,7 +7,7 @@
 class LedSlider : public CentroidDetectionScaled
 {
 public:
-	static constexpr size_t kDefaultNumWeights = 4;
+	static constexpr size_t kDefaultNumWeights = 3;
 	struct centroid_t {
 		float location;
 		float size;
